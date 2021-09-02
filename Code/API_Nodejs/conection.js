dc79dfe2dd35cc43bfd3eb243e656e3d1801731f
hu2g <huriel2gomez@gmail.com>
2021-09-02 15:54:40 -0600

    add

--- Code/API_Nodejs/conection.js
+++ Code/API_Nodejs/conection.js
@@ -1,60 +1,17 @@
-var sql = require("mssql");
+var sql = require("rest-mssql-nodejs");
 
-// credentials for your database
-//const conn = new sql.ConnectionPool({
-const config ={
-    user: 'DESKTOP-L0O2DCR\Huriel Gómez',
-    password: '',
-    server: 'DESKTOP-L0O2DCR',
+const conn = new sql({
+    user: 'sa',
+    password: '1234',
+    server: 'localhost',
     database: 'cine',
-    port: 1433,
-    options:{
-        enableArithAbort:true
-    },
-    connectionTimeout: 3000,//150000,
-    pool:{
-        max: 10,
-        min: 0,
-        idleTimeoutMillis: 30000
-    }
-}
-    //});
-
-sql.on('error', err => {
-    console.log(err.message)
 });
 
-async function getConsulta(){
-    try {
-        let pool = await sql.connect(config);
-        let result1 = await pool.request().query("select * from movie");
-        console.log(result1);
-        sql.close();
-    } catch (error) {
-        console.log(error.message);
-        sql.close();
-    }
-    console.log("fin")
-}
-
-console.log(getConsulta());
-
-
-/*function myQuery(query){
-    sql.connect(credentials, function(err){
-        if(err){ 
-            console.log(err);
-            return err;
-        }
-        var request = new sql.Request()
-        request.query(query, function(err, recordset){
-            if(err){ 
-                console.log(err);
-                return err;
-            }
-            return recordset;
-        });
-    });
-}
+/*
+setTimeout(async () => {
+    const r = await conn.executeQuery("select * from category");
+    console.log(r);
+}, 1500);
 */
-//module.exports = conn;
+
+module.exports = conn;
--- Code/API_Nodejs/routes/cartelera.js
+++ Code/API_Nodejs/routes/cartelera.js
@@ -1,37 +1,100 @@
 const express = require("express");
-const { pool } = require("mssql");
 const router = express.Router();
 const conn = require('../conection');
-//var sql = require("mssql");
 
-
-router.get('/cartelera', (req, res) => {
-    // GET_CARTELERA
+/**
+ * GET_CARTELERA
+ * Peliculas en cartelera  
+ **/ 
+router.get('/cartelera', async (req, res) => {
     const sql = "EXECUTE GET_CARTELERA";
-    console.log (conn.connect());
-    /*const query = conn.query(sql, (err, results) => {
-        if (err) {
-            res.send([]);
-        } else {
-            res.send(results);
-        }
-    });*/
-    /*sql.connect(conn.credentials, function(err){
-        if(err){ 
-            console.log(err);
-            res.send([]);
-        }
-        var request = new sql.Request()
-        request.query(sqlQuery, function(err, recordset){
-            if(err){ 
-                console.log(err);
-                res.send([]);
-            }
-            res.send(recordset);
-        });
-    });*/
-
+    try {
+        const table = await conn.executeQuery(sql);
+        //console.log(r.data);
+        res.send(table.data);    
+    } catch (error) {
+        console.log("Error: "+error.message);
+        res.send([]);
+    }
     res.send("ok..!!!");
+});
+
+/**
+ * FUNCTION_DETAIL
+ * Informacion de las funciones en la que se encuentra una pelicula 
+ * Parameters:
+ * @ID_MOVIE INTEGER: Id de la pelicula
+ **/ 
+router.get('/fundetail', async (req, res) => {
+    const sql = "EXECUTE FUNCTION_DETAIL @id";
+    try {
+        const { idmovie }= req.query;
+        const table = await conn.executeQuery(sql,[{
+            name:'id',
+            type:'int',
+            value: idmovie
+        }]);
+        //console.log(r.data);
+        res.send(table.data);    
+    } catch (error) {
+        console.log("Error: "+error.message);
+        res.send([]);
+    }
+    res.send("ok..!!!");
+});
+
+
+
+/**
+ * OCCUPIED_SEATS
+ * Asientos de la sala ocupados en una funcion
+ * Parameters:
+ * @ID_FUNCTION INTEGER: Id de la funcion
+ */
+router.get('/ocupseats', async (req, res) => {
+    const sql = "EXECUTE OCCUPIED_SEATS @id";
+    try {
+        const { idfunction }= req.query;
+        const table = await conn.executeQuery(sql,[{
+            name:'id',
+            type:'int',
+            value: idfunction
+        }]);
+        //console.log(r.data);
+        res.send(table.data);    
+    } catch (error) {
+        console.log("Error: "+error.message);
+        res.send([]);
+    }
+    res.send("ok..!!!");
+});
+
+
+
+/**
+ * ALL_SEAT_CINEMA
+ * Todos los asientos de una sala
+ * Parameters:
+ * @ID_CINEMA INTEGER: Id de la sala
+ */
+router.get('/allseat', async (req, res) => {
+    const sql = "EXECUTE ALL_SEAT_CINEMA @id";
+    try {
+        const { idsala }= req.query;
+        const table = await conn.executeQuery(sql,[{
+            name:'id',
+            type:'int',
+            value: idsala
+        }]);
+        //console.log(r.data);
+        res.send(table.data);    
+    } catch (error) {
+        console.log("Error: "+error.message);
+        res.send([]);
+    }
+    res.send("ok..!!!");
+
+
 });
 
 
